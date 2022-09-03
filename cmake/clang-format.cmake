6fa6eeb0ac06a76c8ff3b24bbf9631636b17cdab
Johannes Gumbel <johannes[@]gumbel.se>
2022-09-03 06:51:21 -0700

    Don't show processed files when formating with clang-format

--- cmake/clang-format.cmake
+++ cmake/clang-format.cmake
@@ -1,6 +1,6 @@
 set(WELLHEAD_CLANG_FORMAT_COMMAND
     find ${PROJECT_SOURCE_DIR} \( -name \*.cpp -or -name \*.hpp \) -not -ipath \*/third_party/\* |
-        xargs clang-format -i -style=file --verbose
+        xargs clang-format -i -style=file
 )
 
 add_custom_target(clang-format
@@ -9,6 +9,6 @@ add_custom_target(clang-format
 )
 
 add_custom_target(test-clang-format
-    COMMAND ${WELLHEAD_CLANG_FORMAT_COMMAND} -Werror --dry-run
+    COMMAND ${WELLHEAD_CLANG_FORMAT_COMMAND} --verbose -Werror --dry-run
     VERBATIM
 )
